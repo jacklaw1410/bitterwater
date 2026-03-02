c2ed3b59b06a52e6998b1f8f838ada7b16341567
Jack Law <jacklaw1410@gmail.com>
2026-03-02 12:37:03 +0800

    test: update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  webServer: { command: 'bun run build && bun preview', port: 4173 },
+  webServer: { command: process.env.CI ? 'bun run build && bun preview' : 'bun dev --port 4173', port: 4173 },
   testDir: 'e2e',
 });
