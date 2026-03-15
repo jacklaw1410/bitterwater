66e5a357b68b9cce4c1a7541efb89c35211b230d
Jack Law <jacklaw1410@gmail.com>
2026-03-15 22:14:08 +0800

    ci: experiment on playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   webServer: {
     command: process.env.CI ? 'bun run build && bun preview' : 'bun dev --port 4173',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
   },
   use: {
-    baseURL: process.env.CI ? 'http://localhost:4173/bitterwater/' : 'http://localhost:4173',
+    baseURL: process.env.CI ? 'http://localhost:4173/bitterwater/' : 'http://localhost:4173/',
   },
   testDir: 'e2e',
 });
