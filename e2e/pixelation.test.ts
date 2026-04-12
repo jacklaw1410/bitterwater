8829a00846fc6a4a64105496485f20e159dd8b30
Jack Law <jacklaw1410@gmail.com>
2026-04-12 18:42:02 +0800

    test: longer timeout for screenshot test

--- e2e/pixelation.test.ts
+++ e2e/pixelation.test.ts
@@ -46,6 +46,7 @@ test('Pixelation page', async ({ page }) => {
   await expect(page).toHaveScreenshot('pixelation-page-processed.png', {
     fullPage: true,
     maxDiffPixelRatio: 0.01,
+    timeout: 30_000,
   });
 
   const fileChooserPromise = page.waitForEvent('filechooser');
@@ -60,6 +61,7 @@ test('Pixelation page', async ({ page }) => {
   await expect(page).toHaveScreenshot('pixelation-page-uploaded.png', {
     fullPage: true,
     maxDiffPixelRatio: 0.01,
+    timeout: 30_000,
   });
 
   await expect(resetButton).toBeEnabled();
@@ -72,5 +74,6 @@ test('Pixelation page', async ({ page }) => {
   await expect(page).toHaveScreenshot('pixelation-page-reset.png', {
     fullPage: true,
     maxDiffPixelRatio: 0.01,
+    timeout: 30_000,
   });
 });
